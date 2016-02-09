92b36f4a679173d82d8c6bf11d9693e06a6e2677
SteveSnow <Steven.Snow@outlook.com>
2016-02-09 15:56:09 -0500

    refactored js

--- scripts/send_number.js
+++ scripts/send_number.js
@@ -1,13 +1,12 @@
 function sendNumber(num){
-var screenVal =document.getElementById("screenDisplay");
-if (screenVal.innerHTML == 0 && screenVal.innerHTML !=="0."){
-if (num !=="."){
-screenVal.innerHTML=num
-}else{
-screenVal.innerHTML="0."
-}
-}else {
-var newVal=screenVal.innerHTML + num
-screenVal.innerHTML=newVal
-};
+	var screenVal =document.getElementById("screenDisplay");
+	if (screenVal.innerHTML == 0 && screenVal.innerHTML !=="0."){
+		if (num !=="."){
+			screenVal.innerHTML=num
+		}else{
+			screenVal.innerHTML="0."
+		}
+	}else {
+		var newVal=screenVal.innerHTML + num
+		screenVal.innerHTML=newVal};
 }
